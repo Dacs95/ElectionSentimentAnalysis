a90150746b4108f812eaebf97284bb17dca0498d
Jumarogu <jmromerog511@gmail.com>
2018-05-02 05:26:34 -0500

    mining and sentimental

--- backend/services/miningGeneralTweets/generalMining.py
+++ backend/services/miningGeneralTweets/generalMining.py
@@ -4,6 +4,14 @@ from pymongo import MongoClient
 import json
 from credentials import *
 
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# Instantiates a client
+clientLanguageService = language.LanguageServiceClient()
+
 usersDic = []
 #Connection to mongo Atlas DataBase
 client = MongoClient("mongodb+srv://Dacs95:blanco12@cluster0-y8r2m.mongodb.net/")
@@ -27,41 +35,75 @@ def connectionTweepy():
     api = tweepy.API(auth)
     return api
     
-def isFromCandidate(tweet, id):
+def isFromCandidate(tweet):
     us = tweet["user"]
-    if us["screen_name"] == id:
-        return True
-    else:
-        usersDic.append(us)
-        return False
+    candidates = ["lopezobrador_", "JoseAMeadeK", "RicardoAnayaC", "Mzavalagc", "JaimeRdzNL"]
+    for candidate in candidates:
+        if us["screen_name"] == candidate:
+            return True
+        else:
+            usersDic.append(us)
+            return False
 
-def saveUsers(users,name):
+def saveUsers(users,screen_name):
     for user in users:
         try:
-            db["users-"+name].insert_one(user)
-        except expression as identifier:
-            print ("there is a problem ",e)
+            db["users"+screen_name].insert_one(user)
+        except Exception as e:
+            print ("there is a problem ", e)
         else:
-            print("User Saved Wiiiiii XDXDXD me vale verga a la verga")
-    
+            print("User Saved")
 
-def mineKeyword(keywords):
+def saveTweets(tweets, screen_name):
+    for tweet in tweets:
+        try:
+            db['users'+screen_name].insert_one(tweet)
+        except Exception as e:
+            print("Problem " + e)
+        else :
+            print("Tweet saved")
+
+def getSentiment(text):
+    return clientLanguageService.analyze_sentiment(document=text).document_sentiment
+
+
+def mineKeyword(keywords, screen_name):
     api = connectionTweepy()
+    count = 0
     for keyword in keywords:
         search_results = api.search(q=keyword , count=100)
         print(type(search_results))
         print(len(search_results))
         for tweet in search_results: 
             ##cleaaan
-            #if(!fromCandidate(tweet))
             json_str = json.dumps(tweet._json)
             jsonTweet = json.loads(json_str)
-            print(jsonTweet['text'])
+            if not isFromCandidate(jsonTweet) :
+                text = jsonTweet["text"]
+                print(text)
+                clean_text = " ".join(item for item in text.split() if not (item.startswith('@') and len(item) > 2))
+                clean_text = " ".join(item for item in clean_text.split() if not (item.startswith('https://') and len(item) > 7))
+                clean_text = " ".join(item for item in clean_text.split() if not (item.startswith('R') and item.endswith('T') and len(item) > 1))
+                clean_text = " ".join(item for item in clean_text.split() if not (item.startswith('#') and len(item) > 2))
 
-mineKeyword(["amlo"])
-## get general tweets with at least 5 keywords
-## get users of tweets, clean candidates tweets
-## validate different user when saving on db
+                jsonTweet["clean_text"] = clean_text
+                # Detects the sentiment of the text
+                sentiment = getSentiment(clean_text)
+                print('Text: {}'.format(clean_text))
+                print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
+                print(jsonTweet)
+                count = count + 1
+                print(count)
+
+    #saveUsers(usersDic, screen_name)
+
+mineKeyword(keywords["lopezobrador_"], "lopezobrador_")
+## get general tweets with at least 5 keywords DONE
+## get users of tweets, clean candidates tweets DONE
+## validate different user when saving on db DONE
+## save users, not candidates
+##clean data RT @USER https://
 ## get tweets with coordenates values
 ## save gentweets about candidate
 
