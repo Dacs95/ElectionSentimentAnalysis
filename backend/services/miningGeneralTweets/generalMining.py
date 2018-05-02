9f026390d27c511d5d3bb193a196928734667273
Jumarogu <jmromerog511@gmail.com>
2018-05-02 03:20:09 -0500

    cleaning data

--- backend/services/miningGeneralTweets/generalMining.py
+++ backend/services/miningGeneralTweets/generalMining.py
@@ -7,13 +7,15 @@ from credentials import *
 #Connection to mongo Atlas DataBase
 client = MongoClient("mongodb+srv://Dacs95:blanco12@cluster0-y8r2m.mongodb.net/")
 db = client["sa-data"]
-["lopezobrador_","JoseAMeadeK","RicardoAnayaC","Mzavalagc","JaimeRdzNL"]
+tweets = {}
+
 keywords = {
-    'lopezobrador_': ["amlo", "lopez obrador", "@lopezobrador_", "peje", "amlove", "#amlo", "morena", "#morena" ], 
-    'JoseAMeadeK': ["meade", "pri", "jose antonio meade", "#primeade", "pri meade", "#meade", "#pri", "pri meade"], 
-    'RicardoAnayaC': ["anaya", "ricardo anaya", "pan", "#pan", "#ricardoanaya", "#anaya", "pan anaya" ], 
-    'Mzavalagc': [],
-    'JaimeRdzNL': [] }
+    'lopezobrador_': ["amlo", "lopez obrador", "@lopezobrador_", "peje", "#amlove", "#amlo", "morena", "#morena", "amlono", "amlove"], 
+    'JoseAMeadeK': ["meade", "pri", "jose antonio meade", "@JoseAMeadeK", "#primeade", "pri meade", "#meade", "#pri", "pri meade", "#yomero", "#avanzarcontigo"], 
+    'RicardoAnayaC': ["anaya", "ricardo anaya", "@RicardoAnayaC", "pan", "#pan", "#ricardoanaya", "#anaya", "pan anaya", "#prian", "#conanayapormexico", "#defrentealfuturo"], 
+    'Mzavalagc': ["margarita", "independiente", "@Mzavalagc", "#MargaritaNosConvieneATODOS", "margarita zavala", "maz", "calderon-zavala", "la señora calderon", "zabala"], 
+    'JaimeRdzNL': ["bronco", "@JaimeRdzNL", "jaime rodriguez", "cortele la mano", "jaime rodriguez calderon", "candidato independiente", "#levantatemexico", "#mexicobronco"] 
+    }
 
 def connectionTweepy():
     # Creating the authentication object
@@ -25,18 +27,22 @@ def connectionTweepy():
     return api
 
 def mineKeyword(keywords):
-    
-    for keyword in keywords[]
-    results = api.search(q="lopez obrador" , rpp=100, page=1)
-    for result in results: 
-        print(result.text)
-
     api = connectionTweepy()
+    for keyword in keywords:
+        search_results = api.search(q=keyword , count=100)
+        print(type(search_results))
+        print(len(search_results))
+        for tweet in search_results: 
+            ##cleaaan
+            #if(!fromCandidate(tweet))
+            json_str = json.dumps(tweet._json)
+            jsonTweet = json.loads(json_str)
+            print(jsonTweet['text'])
 
-
-
+mineKeyword(["amlo"])
 ## get general tweets with at least 5 keywords
 ## get users of tweets, clean candidates tweets
+## validate different user when saving on db
 ## get tweets with coordenates values
 ## save gentweets about candidate
 
